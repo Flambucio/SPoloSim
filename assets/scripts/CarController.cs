1c58587e2dc11952c04090a9f916b4d0de3079a5
Flambucio <cribodoni@gmail.com>
2026-05-26 17:37:16 +0200

    Refactor CarController.cs by removing comments
    Removed commented code and improved code clarity.

--- assets/scripts/CarController.cs
+++ assets/scripts/CarController.cs
@@ -3,7 +3,6 @@ using System;
 
 public partial class CarController : VehicleBody3D
 {
-	// Called when the node enters the scene tree for the first time.
 	[Export] public float MaxTorque = 600.0f*10;
 	[Export] public float SteeringLimit = 0.5f;
 	[Export] public float BrakeForce = 10.0f;
@@ -12,7 +11,6 @@ public partial class CarController : VehicleBody3D
 
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
@@ -20,18 +18,13 @@ public partial class CarController : VehicleBody3D
 	public override void _PhysicsProcess(double delta)
 	{
 		float fDelta = (float)delta;
-
-		// Accelerazione e Retromarcia
-		// Input.GetAxis restituisce un valore tra -1 e 1
 		float throttle = Input.GetAxis("Retro", "Accelera");
 		float multiplier = Input.IsActionPressed("Turbo") ? 10 : 1;
 		EngineForce = throttle * MaxTorque * multiplier;
 
-		// Sterzata con interpolazione (Lerp) per fluidità
 		float steerTarget = Input.GetAxis("SterzaDestra", "SterzaSinistra") * SteeringLimit;
 		Steering = Mathf.Lerp(Steering, steerTarget, 10.0f * fDelta);
 
-		// Freno (Spazio)
 		if (Input.IsActionPressed("Frena"))
 		{
 			Brake = BrakeForce;
